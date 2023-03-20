acf1cf8b42f70a231c4224a291435f788d6752ef
SamuelGSa <sgmsa@icloud.com>
2023-03-20 20:20:54 -0300

    before e after

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -5,9 +5,7 @@ import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.exception.FilmeSemEstoqueException;
 import br.ce.wcaquino.exception.LocadoraExpection;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
 
@@ -20,17 +18,22 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LocacaoServiceTest {
-
+	LocacaoService service;
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
 
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
+	private Usuario usuarioteste;
+
+	@Before
+	public void setup() {
+		service = new LocacaoService();
+	}
 
 	@Test
 	public void test_valida_valor_e_data_locacao() throws Exception {
 		//Cenario
-		LocacaoService service = new LocacaoService();
 		Usuario usuarioteste = new Usuario("Usuario 1");
 		Filme filmeTeste = new Filme("Filme1", 2, 5.0);
 		
@@ -38,35 +41,26 @@ public class LocacaoServiceTest {
 		Locacao locacao = service.alugarFilme(usuarioteste, filmeTeste);
 		
 		//Verificacao
-
 		Assert.assertThat(locacao.getValor(), is(equalTo(5.0)));
 		assertThat(isMesmaData(locacao.getDataLocacao(),new Date()),is(true));
 		assertThat(isMesmaData(locacao.getDataRetorno(),obterDataComDiferencaDias(1)),is(true));
-
-		// Dessa forma é especificado a quantidade de  erros ao executar o teste.
-//		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
-//		error.checkThat(isMesmaData(locacao.getDataLocacao(),new Date()),is(true));
-//		error.checkThat(isMesmaData(locacao.getDataRetorno(),obterDataComDiferencaDias(1)),is(true));
 	}
 
 // 3 formas de tratar uma exception com JUnit
 	@Test(expected= FilmeSemEstoqueException.class)
 	public void teste_deve_retornar_exception_elegante() throws Exception {
 		//Cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuarioteste = new Usuario("Usuario 1");
+		usuarioteste = new Usuario("Usuario 1");
 		Filme filmeTeste = new Filme("Filme1", 0, 5.0);
 
 		//Acao
 		service.alugarFilme(usuarioteste, filmeTeste);
 
-		// Forma Elegante = passa na Annotation que é esperado uma Exception.class
-	}
+	}// Forma Elegante = passa na Annotation que é esperado uma Exception.class
 
 	@Test
 	public void teste_usuario_vazio() throws FilmeSemEstoqueException {
 		//cenario
-		LocacaoService service = new LocacaoService();
 		Filme filmeTeste = new Filme("Filme1", 2, 5.0);
 
 		//acao
@@ -77,13 +71,11 @@ public class LocacaoServiceTest {
 		 catch (LocadoraExpection e) {
 			Assert.assertThat(e.getMessage(),is("Usuario Vazio"));
 		}
-		// Forma Robusta
-	}
+	}// Forma Robusta
 
 	@Test
 	public void teste_filme_vazio() throws LocadoraExpection, FilmeSemEstoqueException {
 		//cenario
- 		LocacaoService service = new LocacaoService();
 		Usuario usuarioteste = new Usuario("Usuario 1");
 
 		expectedException.expect(LocadoraExpection.class);
@@ -91,6 +83,5 @@ public class LocacaoServiceTest {
 
 		//acao
 		service.alugarFilme(usuarioteste, null);
-	}
-	// Forma Nova
+	}// Forma Nova
 }
